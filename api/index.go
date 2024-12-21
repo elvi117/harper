3a1fec7035486f1dfeb1ed9a859006adc7fef6e0
Lukasz Matuszczak <matuszczak.lukas@gmail.com>
2024-12-21 20:05:49 +0100

    test

--- api/index.go
+++ api/index.go
@@ -1,36 +1,10 @@
 package handler
-
+ 
 import (
-	"harper/api/auth"
-	"net/http"
-	"text/template"
+  "fmt"
+  "net/http"
 )
-
-var tpl *template.Template
-
-func Handler() {
-	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
-	tpl, _ = template.ParseGlob("./templates/*html")
-	// Public handlers
-	http.HandleFunc("/logowanie", logInPage)
-	http.HandleFunc("/utworz_konto", signUpPage)
-	http.HandleFunc("/reset_hasla", resetPasswordPage)
-	http.HandleFunc("/utworz_haslo", createPasswordPage)
-
-	// Private handlers
-	http.HandleFunc("/", accountStatusPage)
-	http.HandleFunc("/konto", myAccountPage)
-	http.HandleFunc("/zasady", rulesPage)
-	http.HandleFunc("/nagrody", prizesPage)
-	http.HandleFunc("/produkty", productsPage)
-
-	// Methods
-	http.HandleFunc("/log_in", auth.LogIn)
-	http.HandleFunc("/sign_up", auth.SingUp)
-	http.HandleFunc("/reset_password", auth.ResetPassword)
-	http.HandleFunc("/create_password", auth.CreatePassword)
-	http.HandleFunc("/log_out", auth.LogOut)
-
-	http.ListenAndServe(":8080", nil)
+ 
+func Handler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
 }
